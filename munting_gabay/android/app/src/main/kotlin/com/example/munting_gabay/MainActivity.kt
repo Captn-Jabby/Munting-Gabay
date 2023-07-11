ba6faa4d20294e90cdca98351ca07773fa4274ca
Prince Rance <ranceprince05@gmail.com>
2023-07-11 23:40:05 +0800

    initial commit

---
+++ munting_gabay/android/app/src/main/kotlin/com/example/munting_gabay/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.munting_gabay
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ munting_gabay/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MuntingGabay());
+}
+class MuntingGabay extends StatelessWidget {
+  const MuntingGabay({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
