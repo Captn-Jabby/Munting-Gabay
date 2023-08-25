b9188b9a4b4e465a7e12cc5baf04d4a486d2dc30
BENEDICT <bened8ct12@gmail.com>
2023-08-26 04:07:02 +0800

    updated firebase

--- munting_gabay/lib/screens/Parent_page.dart
+++ munting_gabay/lib/screens/Parent_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:munting_gabay/parents%20page/psych_search.dart';
 import 'package:munting_gabay/variable.dart';
 
 import 'drawer_page.dart';
@@ -36,214 +37,219 @@ class ParentPage extends StatelessWidget {
         ],
       ),
       drawer: AppDrawer(),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              width: 350,
-              height: 40,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black, // Change the color of the outline
-                  width: 2, // Set the width of the outline
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 350,
+                height: 40,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black, // Change the color of the outline
+                    width: 2, // Set the width of the outline
+                  ),
+                  borderRadius: BorderRadius.circular(15),
                 ),
-                borderRadius: BorderRadius.circular(15),
+                child: Center(
+                    child: Text(
+                  "PARENTS PAGE",
+                  style: buttonTextStyle,
+                )),
               ),
-              child: Center(
-                  child: Text(
-                "PARENTS PAGE",
-                style: buttonTextStyle,
-              )),
-            ),
-            SizedBox(height: 50),
-            Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: BtnHeight,
-                      ),
-                      Image.asset(
-                        'assets/LOGO.png',
-                        width: LOGOSIZE,
-                        height: LOGOSIZE,
-                      ),
-                      SizedBox(
-                        width: lOGOSpacing,
-                      ),
-                      Container(
-                        width: BtnWidth,
-                        height: BtnHeight,
-                        decoration: BoxDecoration(
-                          borderRadius:
-                              BorderRadius.circular(BtnCircularRadius),
+              SizedBox(height: 50),
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: BtnHeight,
                         ),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            // TODO: Add Forum and Discussion button functionality here
-                            print('Forum and Discussion button pressed!');
-                          },
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.blue,
-                            onPrimary: Colors.white,
+                        Image.asset(
+                          'assets/LOGO.png',
+                          width: LOGOSIZE,
+                          height: LOGOSIZE,
+                        ),
+                        SizedBox(
+                          width: lOGOSpacing,
+                        ),
+                        Container(
+                          width: BtnWidth,
+                          height: BtnHeight,
+                          decoration: BoxDecoration(
+                            borderRadius:
+                                BorderRadius.circular(BtnCircularRadius),
                           ),
-                          child: Text(
-                            'Forum and Discussion',
-                            style: ParentbuttonTextStyle,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // TODO: Add Forum and Discussion button functionality here
+                              print('Forum and Discussion button pressed!');
+                            },
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.blue,
+                              onPrimary: Colors.white,
+                            ),
+                            child: Text(
+                              'Forum and Discussion',
+                              style: ParentbuttonTextStyle,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: BtnHeight,
-                      ),
-                      Container(
-                        width: BtnWidth,
-                        height: BtnHeight,
-                        decoration: BoxDecoration(
-                          borderRadius:
-                              BorderRadius.circular(BtnCircularRadius),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: BtnHeight,
                         ),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            // TODO: Add Educational Webinars button functionality here
-                            print('Educational Webinars button pressed!');
-                          },
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.green,
-                            onPrimary: Colors.white,
+                        Container(
+                          width: BtnWidth,
+                          height: BtnHeight,
+                          decoration: BoxDecoration(
+                            borderRadius:
+                                BorderRadius.circular(BtnCircularRadius),
                           ),
-                          child: Text(
-                            'Educational Webinars',
-                            style: ParentbuttonTextStyle,
-                          ),
-                        ),
-                      ),
-                      Image.asset(
-                        'assets/LOGO.png',
-                        width: LOGOSIZE,
-                        height: LOGOSIZE,
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: BtnHeight,
-                      ),
-                      Image.asset(
-                        'assets/LOGO.png',
-                        width: LOGOSIZE,
-                        height: LOGOSIZE,
-                      ),
-                      Container(
-                        width: BtnWidth,
-                        height: BtnHeight,
-                        decoration: BoxDecoration(
-                          borderRadius:
-                              BorderRadius.circular(BtnCircularRadius),
-                        ),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            // TODO: Add Resource Library button functionality here
-                            print('Resource Library button pressed!');
-                          },
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.orange,
-                            onPrimary: Colors.white,
-                          ),
-                          child: Text(
-                            'Resource Library',
-                            style: ParentbuttonTextStyle,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // TODO: Add Educational Webinars button functionality here
+                              print('Educational Webinars button pressed!');
+                            },
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.green,
+                              onPrimary: Colors.white,
+                            ),
+                            child: Text(
+                              'Educational Webinars',
+                              style: ParentbuttonTextStyle,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: BtnHeight,
-                      ),
-                      Container(
-                        width: BtnWidth,
-                        height: BtnHeight,
-                        decoration: BoxDecoration(
-                          borderRadius:
-                              BorderRadius.circular(BtnCircularRadius),
+                        Image.asset(
+                          'assets/LOGO.png',
+                          width: LOGOSIZE,
+                          height: LOGOSIZE,
                         ),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            // TODO: Add Online Consultation button functionality here
-                            print('Online Consultation button pressed!');
-                          },
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.purple,
-                            onPrimary: Colors.white,
-                          ),
-                          child: Text(
-                            'Online Consultation',
-                            style: ParentbuttonTextStyle,
-                          ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: BtnHeight,
                         ),
-                      ),
-                      Image.asset(
-                        'assets/LOGO.png',
-                        width: LOGOSIZE,
-                        height: LOGOSIZE,
-                      ),
-                      SizedBox(
-                        width: lOGOSpacing,
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: BtnHeight,
-                      ),
-                      Image.asset(
-                        'assets/LOGO.png',
-                        width: LOGOSIZE,
-                        height: LOGOSIZE,
-                      ),
-                      SizedBox(
-                        width: lOGOSpacing,
-                      ),
-                      Container(
-                        width: BtnWidth,
-                        height: BtnHeight,
-                        decoration: BoxDecoration(
-                          borderRadius:
-                              BorderRadius.circular(BtnCircularRadius),
+                        Image.asset(
+                          'assets/LOGO.png',
+                          width: LOGOSIZE,
+                          height: LOGOSIZE,
                         ),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            // TODO: Add Psychologist Research button functionality here
-                            print('Psychologist Research button pressed!');
-                          },
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.red,
-                            onPrimary: Colors.white,
+                        Container(
+                          width: BtnWidth,
+                          height: BtnHeight,
+                          decoration: BoxDecoration(
+                            borderRadius:
+                                BorderRadius.circular(BtnCircularRadius),
                           ),
-                          child: Text(
-                            'Psychologist Research',
-                            style: ParentbuttonTextStyle,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // TODO: Add Resource Library button functionality here
+                              print('Resource Library button pressed!');
+                            },
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.orange,
+                              onPrimary: Colors.white,
+                            ),
+                            child: Text(
+                              'Resource Library',
+                              style: ParentbuttonTextStyle,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: BtnHeight,
+                        ),
+                        Container(
+                          width: BtnWidth,
+                          height: BtnHeight,
+                          decoration: BoxDecoration(
+                            borderRadius:
+                                BorderRadius.circular(BtnCircularRadius),
+                          ),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // TODO: Add Online Consultation button functionality here
+                              print('Online Consultation button pressed!');
+                            },
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.purple,
+                              onPrimary: Colors.white,
+                            ),
+                            child: Text(
+                              'Online Consultation',
+                              style: ParentbuttonTextStyle,
+                            ),
+                          ),
+                        ),
+                        Image.asset(
+                          'assets/LOGO.png',
+                          width: LOGOSIZE,
+                          height: LOGOSIZE,
+                        ),
+                        SizedBox(
+                          width: lOGOSpacing,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: BtnHeight,
+                        ),
+                        Image.asset(
+                          'assets/LOGO.png',
+                          width: LOGOSIZE,
+                          height: LOGOSIZE,
+                        ),
+                        SizedBox(
+                          width: lOGOSpacing,
+                        ),
+                        Container(
+                          width: BtnWidth,
+                          height: BtnHeight,
+                          decoration: BoxDecoration(
+                            borderRadius:
+                                BorderRadius.circular(BtnCircularRadius),
+                          ),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Psychpage()),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.red,
+                              onPrimary: Colors.white,
+                            ),
+                            child: Text(
+                              'Psychologist Research',
+                              style: ParentbuttonTextStyle,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
