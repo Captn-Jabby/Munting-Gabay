32d32d40d12ba6a34fb1c58b05977539dbb96d76
RjMadelo <madelorutherjohn12@gmail.com>
2023-10-22 22:36:10 +0800

    Added Forums page with comment

--- munting_gabay/lib/all screen related to the patients/screens/kids page/Kids_page.dart
+++ munting_gabay/lib/all screen related to the patients/screens/kids page/Kids_page.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:munting_gabay/variable.dart';
 import '../../../Doctors screen/movies_screen.dart';
 import '../../../drawer_page.dart';
-import 'games/main.dart';
+import 'games/categories.dart';
+
 
 class KidsPage extends StatelessWidget {
   @override
@@ -58,7 +59,7 @@ class KidsPage extends StatelessWidget {
                         child: ElevatedButton(
                           onPressed: () {
                             // TODO: Add Games button functionality here
-                            Navigator.push(context, CupertinoPageRoute(builder: (context)=>const KiddieHome()));
+                            Navigator.push(context, CupertinoPageRoute(builder: (context)=>const Categories()));
                             print('Games button pressed!');
                           },
                           style: ElevatedButton.styleFrom(
