fb69457959e40b6e314824445d3803862c995d13
BENEDICT <bened8ct12@gmail.com>
2024-03-06 21:36:05 +0800

    .

--- munting_gabay/lib/all screen related to the patients/screens/kids page/video_player.dart
+++ munting_gabay/lib/all screen related to the patients/screens/kids page/video_player.dart
@@ -14,6 +14,7 @@ class VideoPlayerScreen extends StatefulWidget {
 class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   late VideoPlayerController _controller;
   late Future<void> _initializeVideoPlayerFuture;
+  double _sliderValue = 0.0;
 
   @override
   void initState() {
@@ -23,12 +24,22 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
     _initializeVideoPlayerFuture.catchError((error) {
       print('Video player initialization error: $error');
     });
+    _controller.addListener(() {
+      setState(() {
+        _sliderValue = _controller.value.position.inSeconds.toDouble();
+      });
+    });
   }
 
   @override
   void dispose() {
     _controller.dispose();
     super.dispose();
+  }
+
+  void _seekToSecond(double second) {
+    Duration newDuration = Duration(seconds: second.toInt());
+    _controller.seekTo(newDuration);
   }
 
   @override
@@ -39,9 +50,26 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
         future: _initializeVideoPlayerFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
-            return AspectRatio(
-              aspectRatio: _controller.value.aspectRatio,
-              child: VideoPlayer(_controller),
+            return Column(
+              children: [
+                AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
+                ),
+                Slider(
+                  value: _sliderValue,
+                  min: 0.0,
+                  max: _controller.value.duration.inSeconds.toDouble(),
+                  onChanged: (value) {
+                    setState(() {
+                      _sliderValue = value;
+                    });
+                  },
+                  onChangeEnd: (value) {
+                    _seekToSecond(value);
+                  },
+                ),
+              ],
             );
           } else {
             return Center(child: CircularProgressIndicator());
