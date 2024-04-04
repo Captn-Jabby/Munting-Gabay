608583811fe36f9a61e4429bfd0b637e0e921b13
Captn_Jabby <ranceprince05@gmail.com>
2024-04-05 00:08:43 +0800

    Getting Started additional contents

--- munting_gabay/lib/GettingStarted/content_model.dart
+++ munting_gabay/lib/GettingStarted/content_model.dart
@@ -8,22 +8,20 @@ class UnbordingContent {
 
 List<UnbordingContent> contents = [
   UnbordingContent(
-      title: 'Quality Food',
-      image: 'images/quality.svg',
-      discription: "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the "
-          "industry's standard dummy text ever since the 1500s, "
-          "when an unknown printer took a galley of type and scrambled it "
+      title: 'What is Munting Gabay?',
+      image: 'assets/images/2.png',
+      discription: "Munting Gabay is a mobile application designed to provide comprehensive support"
+          " and resources for individuals with Autism Spectrum Disorder (ASD) and their families. "
+          "Its primary goal is to enhance awareness, offer guidance, and foster inclusion for those affected by ASD."
   ),
   UnbordingContent(
-      title: 'Fast Delevery',
-      image: 'images/delevery.svg',
-      discription: "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the "
-          "industry's standard dummy text ever since the 1500s, "
-          "when an unknown printer took a galley of type and scrambled it "
+      title: 'Welcome to Munting Gabay',
+      image: 'assets/images/2.png',
+      discription: "Thank you for choosing us to support you on your journey."
   ),
   UnbordingContent(
       title: 'Reward surprises',
-      image: 'images/reward.svg',
+      image: 'assets/images/2.png',
       discription: "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the "
           "industry's standard dummy text ever since the 1500s, "
           "when an unknown printer took a galley of type and scrambled it "
--- munting_gabay/lib/GettingStarted/getting_started.dart
+++ munting_gabay/lib/GettingStarted/getting_started.dart
@@ -27,7 +27,6 @@ class _GettingStartedState extends State<GettingStarted> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: scaffoldBgColor,
       body: Column(
         children: [
           Expanded(
@@ -42,29 +41,35 @@ class _GettingStartedState extends State<GettingStarted> {
               itemBuilder: (_, i) {
                 return Padding(
                   padding: const EdgeInsets.all(40),
-                  child: Column(
-                    children: [
-                      // SvgPicture.asset(
-                      //   contents[i].image,
-                      //   height: 300,
-                      // ),
-                      Text(
-                        contents[i].title,
-                        style: TextStyle(
-                          fontSize: 35,
-                          fontWeight: FontWeight.bold,
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        // SvgPicture.asset(
+                        //   contents[i].image,
+                        //   height: 300,
+                        // ),
+
+                        Text(
+                          contents[i].title,
+                          style: TextStyle(
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      SizedBox(height: 20),
-                      Text(
-                        contents[i].discription,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.grey,
-                        ),
-                      )
-                    ],
+                        SizedBox(height: 20),
+                        Text(
+
+                          contents[i].discription,
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+
+                            fontSize: 18,
+                            color: Colors.grey,
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 );
               },
